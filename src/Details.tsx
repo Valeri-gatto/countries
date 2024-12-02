eb959deb8af3cfb514c11addc2f828de4b3949e9
Valeria <none@example.com>
2024-12-03 01:52:44 +0400

    получаю новую api для details

--- src/Details.tsx
+++ src/Details.tsx
@@ -1,11 +1,33 @@
 import { useParams } from "react-router-dom";
+import { Convert, GeneralInfo } from "./parse_details";
 import Arrow from "./assets/icons/arrow-left.svg?react";
+import { useEffect, useState } from "react";
 
 export default function Details() {
     const { country } = useParams();
+    const [info, setInfo] = useState<GeneralInfo | undefined>(undefined);
     // if (country === undefined || !genres_list.includes(country)) {
     //     return <Notfoundpage />
     // }
+
+    async function getAPI() {
+        const res = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+        // https://restcountries.com/v3.1/all?fields=name,flags,population,capital,region,currencies,subregion,tld,languages,borders,cca3
+        if (!res.ok) {
+            throw new Error(`Error ${res}` + `,  ${res.status}`);
+        }
+        const generalInfo = Convert.toGeneralInfo(await res.text());
+        return generalInfo;
+    };
+
+    useEffect(() => {
+        async function getGeneralInfo() {
+            const data = await getAPI();
+            setInfo(data);
+        }
+        getGeneralInfo();
+    }, [])
+
 
     return (
         <>
